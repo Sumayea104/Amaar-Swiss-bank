e239993baa35fc80b19ad6319701400d0fc6910c
Sumayea Rahman <shorbonam104@gmail.com>
2022-09-28 18:00:16 +0600

    error cleard

--- js/deposit.js
+++ js/deposit.js
@@ -4,8 +4,14 @@ document.getElementById("btn-deposit").addEventListener("click",function(){
     // console.log("deposit button clicked")
 
     // step-2 get the deposit amount from the deposit input field
+    // for input field use .value to the value inside the input field
+
     const depositField = document.getElementById("deposit-field");
-    const depositAmount = depositField.Value;
+    const depositAmount = depositField.value;
+    
     console.log(depositAmount);
+    
+
+
 
 })
